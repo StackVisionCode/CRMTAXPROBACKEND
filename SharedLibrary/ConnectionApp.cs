7bdd13f65f2b88e9450771a0550f19a6cea30e97
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2025-05-18 22:22:39 -0400

    Middleware completed.

--- SharedLibrary/ConnectionApp.cs
+++ SharedLibrary/ConnectionApp.cs
@@ -6,7 +6,7 @@ public class ConnectionApp
     public string User { get; }
     public string Password { get; }
 
-    public ConnectionApp(string server = "DESKTOP-S0SEBP1", string user = "ccastillo", string password = "Murasaki2527@")
+    public ConnectionApp(string server = "", string user = "", string password = "")
     {
         Server = server;
         User = user;
