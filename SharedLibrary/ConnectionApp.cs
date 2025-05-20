9ed0697f683342007bb8fc0449a5bb5ea661f03f
Jp12wag <wagneralcantara36@gmail.com>
2025-05-20 11:35:28 -0400

    conexion string server

--- SharedLibrary/ConnectionApp.cs
+++ SharedLibrary/ConnectionApp.cs
@@ -6,11 +6,7 @@ public class ConnectionApp
     public string User { get; }
     public string Password { get; }
 
-<<<<<<< HEAD
-    public ConnectionApp(string server = "localhost\\SQLEXPRESS", string user = "jp", string password = "Jp1212@11")
-=======
     public ConnectionApp()
->>>>>>> 4a3bb8af076a518c3ba1701db66c0b1b83ba6579
     {
         Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "DESKTOP-S0SEBP1";
 
