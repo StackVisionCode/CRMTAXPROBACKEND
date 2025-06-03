018c278d81be8b3fd60a6ffbefeafa99c11ed065
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2025-06-03 15:47:58 -0400

    Ports changed on the ocelots and all projects.

--- SharedLibrary/ConnectionApp.cs
+++ SharedLibrary/ConnectionApp.cs
@@ -8,7 +8,7 @@ public class ConnectionApp
 
     public ConnectionApp()
     {
-        Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "TIC-LAP-09\\SQLEXPRESS";
+        Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "WIN-MUKVKVN5NB1";
 
         User = Environment.GetEnvironmentVariable("DB_USER") ?? "sa";
 
