363a267cead45b30450c89b52868999d923a1f5e
Jorge Turbi <91299371+JorgeTurbi@users.noreply.github.com>
2025-06-08 14:48:33 -0400

    deletedApplicationDbContextFactory.cs

--- SharedLibrary/ConnectionApp.cs
+++ SharedLibrary/ConnectionApp.cs
@@ -8,7 +8,7 @@ public class ConnectionApp
 
     public ConnectionApp()
     {
-        Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "localhost";
+        Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "dev001";
 
         User = Environment.GetEnvironmentVariable("DB_USER") ?? "sa";
 
