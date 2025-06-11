e4edba4a037e547c7204b7386a9c554be8be6ae1
Jp12wag <wagneralcantara36@gmail.com>
2025-06-10 21:35:58 -0400

    db server

--- SharedLibrary/ConnectionApp.cs
+++ SharedLibrary/ConnectionApp.cs
@@ -8,7 +8,7 @@ public class ConnectionApp
 
     public ConnectionApp()
     {
-        Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "WIN-MUKVKVN5NB1";
+        Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "SIP\\SQLEXPRESS";
 
         User = Environment.GetEnvironmentVariable("DB_USER") ?? "sa";
 
