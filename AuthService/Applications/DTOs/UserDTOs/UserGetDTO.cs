9d86dd8720bdece947e06533a834009a2cf188e5
castilloz671 <158379076+castilloz671@users.noreply.github.com>
2025-07-17 21:55:14 -0400

    Fixing GetUserById

--- AuthService/Applications/DTOs/UserDTOs/UserGetDTO.cs
+++ AuthService/Applications/DTOs/UserDTOs/UserGetDTO.cs
@@ -7,7 +7,15 @@ public class UserGetDTO
     [Key]
     public Guid Id { get; set; }
     public Guid? CompanyId { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string? Name { get; set; }
+    public string? LastName { get; set; }
+    public string? Address { get; set; }
+    public string? PhotoUrl { get; set; }
+    public string? Domain { get; set; }
     public ICollection<string> RoleNames { get; set; } = new List<string>();
     public string? FullName { get; set; }
-    public required string Email { get; set; }
+    public string? Phone { get; set; }
+    public string? CompanyName { get; set; }
+    public string? CompanyBrand { get; set; }
 }
